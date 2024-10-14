b3adc134f2b5e6f40bdccc453976ee49364b636f
Steven Zhou <stevenz@usc.edu>
2024-10-14 16:30:00 -0700

    redundant code cleanup

--- Char_recognition model.py
+++ Char_recognition model.py
@@ -8,7 +8,7 @@ import joblib
 from skimage.io import imread
 from skimage.filters import threshold_otsu
 
-#define all the possible letters in a license plate in the list
+#define all the possible letters in a license plate in the listS
 letters = [
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
             'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
